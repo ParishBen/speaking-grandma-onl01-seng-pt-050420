769f4fac1253a329c9fec8121a82e5477750e7bb
Ben Parish <parishben13@gmail.com>
2020-05-05 05:11:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(words)
   return "I LOVE YOU TOO PUMPKIN!"
   elsif words.downcase
    return "HUH?! SPEAK UP, SONNY!"
-  elsif !words.downcase
+  elsif words.upcase
     return "NO, NOT SINCE 1938!"
  end
 end

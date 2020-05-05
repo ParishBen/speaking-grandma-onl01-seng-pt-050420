a9fa6f7cfc28d630555ca1830278401c67c2a1f1
Ben Parish <parishben13@gmail.com>
2020-05-05 05:16:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(words)
    if words == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-  elsif words==words.downcase
-   return "HUH?! SPEAK UP, SONNY!"
   elsif words==words.upcase
     return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
  end
 end
 # Whatever you say to grandma, she should respond with
